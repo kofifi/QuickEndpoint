6e93793864d44e5279bc03b8db2c65dcba3d42db
kofifi <kofirlej@gmail.com>
2024-02-29 22:37:53 +0100

    Changes to gui

--- QuickEndpoint_Gui/ViewModels/MainViewModel.cs
+++ QuickEndpoint_Gui/ViewModels/MainViewModel.cs
@@ -1,5 +1,22 @@
-﻿namespace QuickEndpoint_Gui.ViewModels;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 
-public class MainViewModel : ViewModelBase
+namespace QuickEndpoint_Gui.ViewModels;
+
+public partial class MainViewModel : ObservableObject
 {
+    private bool _isPaneOpen;
+
+    public bool IsPaneOpen
+    {
+        get => _isPaneOpen;
+        set => SetProperty(ref _isPaneOpen, value);
+    }
+
+    [ICommand]
+    private void TriggerPane()
+    {
+        IsPaneOpen = !IsPaneOpen;
+    }
+
 }
